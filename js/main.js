b3a5ec9b8ac679a5954ce06fc7fdc183e8948d2c
mahedihassan5061 <mahedishassans@gmail.com>
2022-04-06 03:06:35 +0600

    'themeforest-portfolio'

---
+++ js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// navbar variables
+const navbarNav = document.querySelector(".menu-item");
+const navbarToggleBtn = document.querySelector(".menu-btn");
+
+// navbar toggle functionality
+navbarToggleBtn.addEventListener("click", function () {
+  navbarNav.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== SERVICES MODAL ===============*/
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+/* Link active work */
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== LIGHT DARK THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
